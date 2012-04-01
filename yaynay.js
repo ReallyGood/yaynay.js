9db60ab91fecd7ac544db7f369aa56eece06ec59
Ronny Orbach <r@ronnyo.com>
2012-04-01 14:30:52 +0300

    some documentation

--- yaynay.js
+++ yaynay.js
@@ -1,4 +1,15 @@
-/* yaynay, a tiny "feature detection" (actually a UA string parser) helper. Heavily inspirted by Modernizr */
+/*! yaynay.js, MIT by http://reallygood.co.il */
+/*
+	After including this (or better yet, the minified) file, specify arrays of disallowed UA regexps, for example:
+	  yaynay.features.volume = [/ipad/, /iphone/, /ipod/, /android(?!.*?mobile)/, /android.*?mobile/, /blackberry/, /windows ce/, /webos/, /playbook/];
+	  yaynay.features.timetravel = [/\w/];
+
+	then you can happily do
+	  if( !yaynay('timetravel') ) alert('no time travel for you, sir!');
+
+	Notice: yaynay isn't "magic" - It doesn't add any classes, variables or whatever, nowhere, but instead just works.
+*/
+
 var yaynay = (function(){
 	var features = {},
 		test = function(feature){
@@ -18,6 +29,3 @@ var yaynay = (function(){
 	test.features = features;
 	return test;
 })();
-
-yaynay.features.volume = [/ipad/, /iphone/, /ipod/, /android(?!.*?mobile)/, /android.*?mobile/, /blackberry/, /windows ce/, /webos/, /playbook/];
-yaynay.features.timetravel = [/\w/];
