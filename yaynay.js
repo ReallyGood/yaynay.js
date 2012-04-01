0a310e89a1983de40b6c241798cf96dd124b349e
Ronny Orbach <r@ronnyo.com>
2012-04-01 14:11:07 +0300

    basic yaynay 'library'

---
+++ yaynay.js
@@ -0,0 +1,23 @@
+/* yaynay, a tiny "feature detection" (actually a UA string parser) helper. Heavily inspirted by Modernizr */
+var yaynay = (function(){
+	var features = {},
+	var test = function(feature){
+		var ua = navigator.userAgent.toLowerCase(),
+			supported = true;
+		if(features[feature]){
+			var blockedUA = features[feature];
+			for(var i = 0, l = blockedUA.length; i < l; i++){
+				if(blockedUA[i].test(ua)) {
+					supported = false;
+					break;
+				}
+			}
+		}
+		return supported;
+	};
+	test.features = features;
+	return test;
+})();
+
+yaynay.features.volume = [/ipad/, /iphone/, /ipod/, /android(?!.*?mobile)/, /android.*?mobile/, /blackberry/, /windows ce/, /webos/, /playbook/];
+yaynay.features.timetravel = [/\w/];
